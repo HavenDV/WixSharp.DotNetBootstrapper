43bcd67be2f594b4bb74e52411cec248fee736bf
Konstantin S <HavenDV@users.noreply.github.com>
2020-10-29 23:57:15 +1000

    Fixed path.

--- src/WixSharp.NetBootstrapper.Web/WebVersions.cs
+++ src/WixSharp.NetBootstrapper.Web/WebVersions.cs
@@ -9,7 +9,7 @@
         /// 
         /// </summary>
         public static NetVersion Net472 { get; } = new NetVersion(
-            @"%userprofile%\.nuget\packages\NSIS-Tool\1.0.0\net-installers\web\NDP472-KB4054531-Web.exe".ExpandEnvVars(), 
+            @"%userprofile%\.nuget\packages\WixSharp.NetBootstrapper.Web\1.0.0\net-installers\web\NDP472-KB4054531-Web.exe".ExpandEnvVars(), 
             @"HKLM:SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.7.2:",
             Condition.Net472_Installed,
             "Please install .NET 4.7.2 first.");
