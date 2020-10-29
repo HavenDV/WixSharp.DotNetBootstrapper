b5fa9043137637849f878b16d8e9694588c5e550
Konstantin S <HavenDV@users.noreply.github.com>
2020-10-30 00:11:04 +1000

    Added FakeCustomActions.

---
+++ src/WixSharp.NetBootstrapper.Web/FakeCustomActions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace WixSharp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EmptyCustomActions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult Action(Session _)
+        {
+            return ActionResult.Success;
+        }
+    }
+}
