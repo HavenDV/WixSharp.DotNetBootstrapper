ae50fa61ed43916198b16be0bc37f7dfdb0d9366
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-23 04:31:19 +1000

    Added NetCore project template.

---
+++ src/WixSharp.DotNetBootstrapper.NetCore/FakeCustomActions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace WixSharp
+{
+    /// <summary>
+    /// This requires only for WixSharp reference.
+    /// </summary>
+    public static class EmptyCustomActions
+    {
+        /// <summary>
+        /// This requires only for WixSharp reference.
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult Action(Session _)
+        {
+            return ActionResult.Success;
+        }
+    }
+}
---
+++ src/WixSharp.DotNetBootstrapper.NetCore/NetCoreVersions.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+
+namespace WixSharp
+{
+    /// <summary>
+    /// Web installers for .Net Framework.
+    /// </summary>
+    public static class NetCoreVersions
+    {
+        #region Constants
+
+        /// <summary>
+        /// .Net Framework 4.8.
+        /// </summary>
+        public static DotNetVersion Net48 { get; } = Create("ndp48-web", "4.8", Condition.Net48_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.7.2.
+        /// </summary>
+        public static DotNetVersion Net472 { get; } = Create("NDP472-KB4054531-Web", "4.7.2", Condition.Net472_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.7.1.
+        /// </summary>
+        public static DotNetVersion Net471 { get; } = Create("NDP471-KB4033344-Web", "4.7.1", Condition.Net471_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.7.
+        /// </summary>
+        public static DotNetVersion Net47 { get; } = Create("NDP47-KB3186500-Web", "4.7", Condition.Net47_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.6.2.
+        /// </summary>
+        public static DotNetVersion Net462 { get; } = Create("NDP462-KB3151802-Web", "4.6.2", Condition.Net462_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.6.1.
+        /// </summary>
+        public static DotNetVersion Net461 { get; } = Create("NDP461-KB3102438-Web", "4.6.1", Condition.Net461_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.6.
+        /// </summary>
+        public static DotNetVersion Net46 { get; } = Create("NDP46-KB3045560-Web", "4.6", Condition.Net46_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.5.2.
+        /// </summary>
+        public static DotNetVersion Net452 { get; } = Create("NDP452-KB2901954-Web", "4.5.2", Condition.Net452_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.5.1.
+        /// </summary>
+        public static DotNetVersion Net451 { get; } = Create("NDP451-KB2859818-Web", "4.5.1", Condition.Net451_Installed);
+
+        /// <summary>
+        /// .Net Framework 4.5.
+        /// </summary>
+        public static DotNetVersion Net45 { get; } = Create("dotNetFx45_Full_setup", "4.5", Condition.Net45_Installed);
+
+        #endregion
+
+        #region Utilities
+
+        private static DotNetVersion Create(string exeName, string version, Condition condition)
+        {
+            return new DotNetVersion(
+                Path.Combine(PackageLocator.GetLatestVersionPath("WixSharp.DotNetBootstrapper.Web"), @$"net-installers\web\{exeName}.exe"),
+                @$"HKLM:SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v{version}:",
+                condition,
+                $"Please install .NET {version} first."
+                );
+        }
+
+        #endregion
+    }
+}
